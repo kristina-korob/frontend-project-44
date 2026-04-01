0bd30ac5a0786ad302d0ac70d88fec4e3c772a3f
korobeinikova <kristunidze@mail.ru>
2026-04-01 13:38:13 +0700

    add

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,7 +3,3 @@
 
 console.log('Welcome to the Brain Games!')
 
-import readlineSync from 'readline-sync';
-
-const name = readlineSync.question('May I have your name? ')
-console.log(`Hello, ${name}!`)
