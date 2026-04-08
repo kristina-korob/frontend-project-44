0b868e084461b7ff2d060ee0eb7a085c3f31b56c
Trashman452 <osipovkirill452@gmail.com>
2026-04-08 12:56:04 +0700

    add

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -3,25 +3,31 @@ import readlineSync from 'readline-sync';
 const getGcd = (a, b) => (b === 0 ? a : getGcd(b, a % b))
 
 const gcd = () => {
-  console.log('Welcome to the Brain Games!')
-  const name = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${name}!`)
-  console.log('Find the greatest common divisor of given numbers.')
+    console.log('Welcome to the Brain Games!')
+    const name = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${name}!`)
+    console.log('Find the greatest common divisor of given numbers.')
 
-  const num1 = Math.floor(Math.random() * 100) + 1
-  const num2 = Math.floor(Math.random() * 100) + 1
 
-  console.log(`Question: ${num1} ${num2}`)
-  const userAnswer = readlineSync.question('Your answer: ')
+    for (let i = 1; i <= 3; i++) {
+        const num1 = Math.floor(Math.random() * 100) + 1
+        const num2 = Math.floor(Math.random() * 100) + 1
 
-  const result = getGcd(num1, num2)
+        console.log(`Question: ${num1} ${num2}`)
+        const userAnswer = readlineSync.question('Your answer: ')
 
-  if (userAnswer === String(result)) {
-    console.log('Correct!')
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.`)
-    console.log(`Let's try again, ${name}!`)
-  }
+        const result = getGcd(num1, num2)
+
+        if (userAnswer === String(result)) {
+            console.log('Correct!')
+        } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.`)
+            console.log(`Let's try again, ${name}!`)
+            return
+        }
+    }
+
+    console.log(`Congratulations, ${name}!`)
 }
 
 gcd()
